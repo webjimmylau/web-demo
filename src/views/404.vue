224d3f655ffde50c9126aeba704ea47fdc1d76bd
lzm <lzm@lesso.com>
2022-02-16 18:32:10 +0800

    fix 404 page

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="page-404">
-    <div class="page-result">
-      <p class="text-404">404</p>
-      <p class="text-no-found">找不到该页面</p>
-      <router-link to="/" class="text-return-index">返回首页</router-link>
-    </div>
+    <p class="text-404">404</p>
+    <p class="text-no-found">找不到该页面</p>
+    <router-link to="/" class="text-return-index">返回首页</router-link>
   </div>
 </template>
 
@@ -19,36 +17,28 @@ export default {}
   bottom: 0;
   left: 0;
   right: 0;
-  padding: 100px 0;
   text-align: center;
   background: @colorGrayF;
   z-index: 999;
-  .page-result {
-    width: 400px;
-    height: 400px;
-    margin: 0 auto;
-    margin-bottom: 30px;
-    background: @colorWhite;
-    border-radius: 100%;
-    overflow: hidden;
-  }
   .text-404 {
-    padding-top: 60px;
+    padding-top: 100px;
     font-size: 128px;
     color: @colorGrayD;
   }
   .text-no-found {
-    padding: 20px 0 100px 0;
+    padding: 20px 0 60px 0;
     font-size: 16px;
     color: @colorGrayLight;
   }
   .text-return-index {
-    display: block;
-    font-size: 16px;
+    display: inline-block;
+    padding: 0 20px;
     line-height: 2;
+    font-size: 16px;
     color: @colorWhite;
     background: @colorGreen;
-    a:hover {
+    border-radius: 3px;
+    &:hover {
       color: @colorWhite;
       opacity: 0.9;
     }
