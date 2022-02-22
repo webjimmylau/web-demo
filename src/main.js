21ed4fbb64bb9cf53c626a6b9d77c0842708e474
lzm <lzm@lesso.com>
2022-02-22 14:29:16 +0800

    add element-ui

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+import './theme/element-ui/index.css'
+
 Vue.config.productionTip = false
 
 import '@/assets/styles/base.css'
 import '@/assets/styles/common.less'
+
+Vue.use(ElementUI)
 
 new Vue({
   router,
